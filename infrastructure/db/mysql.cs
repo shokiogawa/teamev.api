52da6a6f54bafc01a531e28a0e9f402915a1dee2
shokiogawa <syoki2578@outlook.jp>
2022-01-30 20:53:46 +0900

    add: mysql.data取得

---
+++ infrastructure/db/mysql.cs
@@ -0,0 +1,28 @@
+using System;
+using MySql.Data.MySqlClient;
+
+namespace teamev.api.infrastructure.db
+{
+  public class MysqlDb
+  {
+    private static readonly string server = "teamev-db";
+    private static readonly string database = "mysql";
+    private static readonly string user = "user";
+    private static readonly string pass = "secret";
+    private static readonly string charset = "utf8";
+    private static readonly string dsn = string.Format("Server={0};Database={1};Uid={2};Pwd={3};Charset={4}", server, database, user, pass, charset);
+
+    public MySqlConnection mySqlConnection;
+    public MysqlDb()
+    {
+      this.mySqlConnection = new MySqlConnection();
+      mySqlConnection.Open();
+    }
+
+    public MySqlCommand DBConnect()
+    {
+      MySqlCommand command = mySqlConnection.CreateCommand();
+      return command;
+    }
+  }
+}
