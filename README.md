e44892c2c1f812010e46b51f1fdc9c4378e0f412
shokiogawa <syoki2578@outlook.jp>
2022-02-01 21:00:00 +0900

    add: test

--- README.md
+++
@@ -1,1 +0,0 @@
-# teamev.api
--- presentation/dto/ObjectiveDto.cs
+++
@@ -1,9 +0,0 @@
-namespace teamev.api.presentation.dto
-{
-  public class ObjectiveDto
-  {
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public string Author { get; set; }
-  }
-}
