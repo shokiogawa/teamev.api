9bdcb61f8a9149aecf5507c82a25c6c2852182b4
shokiogawa <syoki2578@outlook.jp>
2022-03-04 23:05:48 +0900

    go

--- Startup.cs
+++ Startup.cs
@@ -111,7 +111,7 @@ namespace teamev.api
       {
         Console.WriteLine("Production");
         Console.WriteLine(Configuration["TEST_ENV"]);
-        Console.WriteLine("やあa");
+        Console.WriteLine("来てた");
         Console.WriteLine(Configuration["ConnectionStrings:Default"]);
         Console.WriteLine("こんにちは");
       }
